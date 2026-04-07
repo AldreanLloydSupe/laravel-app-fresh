1e0ecfcf2dc093d7c9fef18b75f9c35032001515
AldreanLloydSupe <a.supe.143998.tc@umindanao.edu.ph>
2026-04-07 17:18:44 +0800

    Initial commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,16 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['firstName',
+            'lastName', 
+            'nickname', 
+            'email', 
+            'age', 
+            'address', 
+            'contact number',
+            ])]
+
+
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
